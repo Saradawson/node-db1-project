5b808308f491057f7a5b0e0ad00e962b51233022
unknown <saradawsone@gmail.com>
2022-10-21 13:06:04 -0600

    check payload middleware working

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -4,20 +4,29 @@ exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
-  if(
-    !req.body.name &&
-    req.body.name.trim() || 
-    !req.body.budget &&
-    !req.body.budget.trim()){
-      res.status(422).json({
-        message: 'name and budget required'
-      })
-    }else{
-      req.account = req.body
-      next()
-    }
-
-}
+  const { name, budget } = req.body
+  if (name === undefined || budget === undefined){
+    res.status(400).json({
+      message: 'name and budget are required'
+    })
+  }else if (typeof name !== 'string'){
+    res.status(400).json({
+      message: 'name of account must be a string'
+    })
+  }else if (name.trim().length < 3 || name.trim().length > 100){
+    res.status(400).json({
+      message: 'name of account must be between 3 and 100'
+    })
+  }else if (typeof budget !== 'number' || isNaN(budget)){
+    res.status(400).json({
+      message: 'budget of account must be a number'
+    })
+  }else if (budget < 0 || budget > 100000000){
+    res.status(400).json({
+      message: 'budget of account is too large or too small'
+    })
+  }
+  }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
